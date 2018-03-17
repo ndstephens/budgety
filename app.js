c58feba8ace533b843b970408152c1a617b7b9d9
Nathaniel Stephens <ndstephens@gmail.com>
2018-03-17 17:19:46 -0400

    Retrieves input values, Adds classname strings to an object

--- app.js
+++ app.js
@@ -5,23 +5,48 @@ var budgetController = (function() {
 
 //?--------- UI CONTROLLER ----------
 var UIController = (function() {
-  // some code later
+  var DOMStrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn',
+  };
+
+  function getInput() {
+    return {
+      type: document.querySelector(DOMStrings.inputType).value, // 'inc' or 'exp'
+      description: document.querySelector(DOMStrings.inputDescription).value,
+      value: document.querySelector(DOMStrings.inputValue).value,
+    };
+  }
+
+  function getDOMStrings() {
+    return DOMStrings;
+  }
+
+  return {
+    getInput: getInput,
+    getDOMStrings: getDOMStrings,
+  };
 })();
 
 //?--------- APP CONTROLLER ----------
 var controller = (function(budgetCtrl, UICtrl) {
+  var DOM = UICtrl.getDOMStrings();
+
   var ctrlAddItem = function() {
-    console.log('click');
     // 1. Get input data
+    var input = UICtrl.getInput();
+    console.log(input);
     // 2. Add item to the budget controller
     // 3. Add item to the UI
     // 4. Calculate the budget
     // 5. Display the budget in the UI
   };
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
-  //* enables the 'ENTER' key to also trigger the input
+  //* enables the 'ENTER' key to also trigger the input (regardless of what's in focus)
   document.addEventListener('keypress', function(event) {
     //* use 'event.which' for older browsers
     if (event.keyCode === 13 || event.which === 13) {
